0d37dc1e2b2a4685f11f5b0e30a78284be667206
deng <rust@maildog.top>
2024-09-24 04:33:36 +0000

    feat(build): add post-build file copy steps
    Copy .traefik.yml and plugin.wasm to demo plugins directory after build.

--- build.sh
+++ build.sh
@@ -3,3 +3,6 @@
 test -f tinygo/bin/tinygo || (curl -LZO https://github.com/tinygo-org/tinygo/releases/download/v0.33.0/tinygo0.33.0.linux-amd64.tar.gz && tar xvzf tinygo0.33.0.linux-amd64.tar.gz)
 
 tinygo/bin/tinygo build -o plugin.wasm -scheduler=none --no-debug -target=wasi .
+
+cp -v .traefik.yml demo/plugins-local/src/github.com/ttys3/traefik-plugin-denyip/
+cp -v plugin.wasm demo/plugins-local/src/github.com/ttys3/traefik-plugin-denyip/
